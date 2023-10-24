031063b054090d4a532d009930ee8b326df7c3a6
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-10-24 14:04:18 -0400

    conver to ORM

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-import requests 
+#import requests 
+from openexchange import OpenExchangeClient
 
 APP_ID = 'cc6fc36a309d4ff8a7a37a0b34f6c265'
-ENDPOINT = 'https://openexchangerates.org/api/latest.json'
 
-response = requests.get(f'{ENDPOINT}?app_id={APP_ID}')
-exchange_rates = response.json()['rates']
+#ENDPOINT = 'https://openexchangerates.org/api/latest.json'
+
+#response = requests.get(f'{ENDPOINT}?app_id={APP_ID}')
+#exchange_rates = response.json()['rates']
+
+client = OpenExchangeClient(APP_ID)
 
 usd_amount = 1000
-gbp_aount = usd_amount * exchange_rates['GBP']
+#gbp_amount = usd_amount * exchange_rates['GBP']
+
+gbp_amount = client.convert(usd_amount, 'USD', 'GBP')
+
+print(f'USD{usd_amount} is GBP{gbp_amount}')
